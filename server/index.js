10223042bced67551e8916c94ed295f24dc300c1
itsZeroFour <itsZeroFourX@gmail.com>
2026-05-06 15:11:18 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,30 @@
+// index.js
+import activemq from 'activemq';
+
+const client = activemq.createClient({
+  host: 'mq-test.maxi-retail.ru',
+  port: 61617,
+  ssl: true,
+  rejectUnauthorized: false,
+  login: 'almuzalewsky',
+  passcode: 'r8D1zYcnFy',
+});
+
+client.connect();
+
+client.on('connect', () => {
+  console.log('Подключились к брокеру!');
+
+  client.send('/queue/test', {}, 'test message');
+  console.log('Сообщение отправлено в очередь');
+
+  client.disconnect();
+});
+
+client.on('error', (err) => {
+  console.error('Ошибка подключения:', err.message);
+});
+
+client.on('disconnect', () => {
+  console.log('Отключились');
+});
