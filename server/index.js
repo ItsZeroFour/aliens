6a0a3b7692ed3e51dadb35fb8c4b6cc2ef03e6f5
itsZeroFour <itsZeroFourX@gmail.com>
2026-05-06 15:34:20 +0300

    change

--- server/index.js
+++ server/index.js
@@ -1,30 +1,30 @@
 // index.js
-import activemq from 'activemq';
+import { createClient } from "activemq";
 
-const client = activemq.createClient({
-  host: 'mq-test.maxi-retail.ru',
+const client = createClient({
+  host: "mq-test.maxi-retail.ru",
   port: 61617,
   ssl: true,
   rejectUnauthorized: false,
-  login: 'almuzalewsky',
-  passcode: 'r8D1zYcnFy',
+  login: "almuzalewsky",
+  passcode: "r8D1zYcnFy",
 });
 
 client.connect();
 
-client.on('connect', () => {
-  console.log('Подключились к брокеру!');
+client.on("connect", () => {
+  console.log("Подключились к брокеру!");
 
-  client.send('/queue/test', {}, 'test message');
-  console.log('Сообщение отправлено в очередь');
+  client.send("/queue/test", {}, "test message");
+  console.log("Сообщение отправлено в очередь");
 
   client.disconnect();
 });
 
-client.on('error', (err) => {
-  console.error('Ошибка подключения:', err.message);
+client.on("error", (err) => {
+  console.error("Ошибка подключения:", err.message);
 });
 
-client.on('disconnect', () => {
-  console.log('Отключились');
+client.on("disconnect", () => {
+  console.log("Отключились");
 });
